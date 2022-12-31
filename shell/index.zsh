923eadfda2fba13c21c8d87d58fbd606566267b8
Rafael Eyng <rafaeleyng@gmail.com>
2022-12-31 12:53:23 -0300

    sign commits (#2)

--- shell/index.zsh
+++ shell/index.zsh
@@ -7,24 +7,24 @@
 # pactl
 ########################################
 # Logitech webcam
-S_INP_RAW="alsa_input.usb-046d_HD_Pro_Webcam_C920_AF6A747F-02.iec958-stereo"
+SOURCE_RAW="alsa_input.usb-046d_HD_Pro_Webcam_C920_AF6A747F-02.iec958-stereo"
 # Display Asus
-S_OUT_RAW="alsa_output.pci-0000_01_00.1.hdmi-stereo-extra1"
+SINK_RAW="alsa_output.pci-0000_01_00.1.hdmi-stereo-extra1"
 
 # Arcano Black
-S_INP_PRO="alsa_input.usb-ARCANO_AM-BLACK-1_ARCANO_AM-BLACK-1-00.mono-fallback"
+SOURCE_PRO="alsa_input.usb-ARCANO_AM-BLACK-1_ARCANO_AM-BLACK-1-00.mono-fallback"
 # Galaxy Buds+
-S_OUT_PRO="bluez_sink.08_BF_A0_D7_01_35.a2dp_sink"
+SINK_PRO="bluez_sink.08_BF_A0_D7_01_35.a2dp_sink"
 
 alias s='pactl'
 
 function configure_sound () {
-  SOUND_INP=$1
-  SOUND_OUT=$2
+  SOURCE=$1
+  SINK=$2
 
-  s set-default-source "$SOUND_INP"
-  s set-default-sink "$SOUND_OUT"
+  s set-default-source "$SOURCE"
+  s set-default-sink "$SINK"
 }
 
-alias s1="configure_sound \"$S_INP_RAW\" \"$S_OUT_RAW\""              # raw
-alias s4="configure_sound \"$S_INP_PRO\" \"$S_OUT_PRO\""              # pro
+alias s1="configure_sound \"$SOURCE_RAW\" \"$SINK_RAW\"" # raw
+alias s4="configure_sound \"$SOURCE_PRO\" \"$SINK_PRO\"" # pro
