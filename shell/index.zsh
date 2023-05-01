bf2ec430c415bf71a744998c4710fe889ddcb924
Rafael Eyng <rafaeleyng@gmail.com>
2023-05-01 16:24:53 -0300

    add new aliases

--- shell/index.zsh
+++ shell/index.zsh
@@ -5,8 +5,44 @@
 ########################################
 alias wezterm='flatpak run org.wezfurlong.wezterm'
 
+# https://github.com/wez/wezterm/issues/207
+__vte_urlencode() (
+  # This is important to make sure string manipulation is handled
+  # byte-by-byte.
+  LC_ALL=C
+  str="$1"
+  while [ -n "$str" ]; do
+    safe="${str%%[!a-zA-Z0-9/:_\.\-\!\'\(\)~]*}"
+    printf "%s" "$safe"
+    str="${str#"$safe"}"
+    if [ -n "$str" ]; then
+      printf "%%%02X" "'$str"
+      str="${str#?}"
+    fi
+  done
+)
+
+__vte_osc7 () {
+  printf "\033]7;file://%s%s\007" "${HOSTNAME:-}" "$(__vte_urlencode "${PWD}")"
+}
+
+__vte_prompt_command() {
+  local command=$(HISTTIMEFORMAT= history 1 | sed 's/^ *[0-9]\+ *//')
+  command="${command//;/ }"
+  local pwd='~'
+  [ "$PWD" != "$HOME" ] && pwd=${PWD/#$HOME\//\~\/}
+  printf "\033]777;notify;Command completed;%s\007\033]0;%s@%s:%s\007%s" "${command}" "${USER}" "${HOSTNAME%%.*}" "${pwd}" "$(__vte_osc7)"
+}
+
+case "$TERM" in
+  xterm*|vte*)
+    [ -n "$BASH_VERSION" ] && PROMPT_COMMAND="__vte_prompt_command"
+    [ -n "$ZSH_VERSION"  ] && precmd_functions+=(__vte_osc7)
+    ;;
+esac
+
 ########################################
-# pactl
+# sound
 ########################################
 # Logitech webcam
 SOURCE_RAW="alsa_input.usb-046d_HD_Pro_Webcam_C920_AF6A747F-02.analog-stereo"
@@ -37,3 +73,10 @@ function configure_sound () {
 
 alias s1="configure_sound \"$SOURCE_RAW\" \"$SINK_RAW\"" # raw
 alias s4="configure_sound \"$SOURCE_PRO\" \"$SINK_PRO\"" # pro
+
+
+########################################
+# clipboard
+########################################
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
